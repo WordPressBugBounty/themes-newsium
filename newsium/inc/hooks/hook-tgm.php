971565ec6989069a6bcf9e71cda98657499a2f2f
Sean Murphy <sean@iamseanmurphy.com>
2024-10-19 10:35:39 +0000

    3.5.0

---
+++ newsium/inc/hooks/hook-tgm.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Recommended plugins
+ *
+ * @package Newsium
+ */
+
+if ( ! function_exists( 'newsium_recommended_plugins' ) ) :
+
+    /**
+     * Recommend plugins.
+     *
+     * @since 1.0.0
+     */
+    function newsium_recommended_plugins() {
+
+        $plugins = array(
+            array(
+                'name'     => esc_html__( 'AF Companion', 'newsium' ),
+                'slug'     => 'af-companion',
+                'required' => false,
+            ),
+            array(
+                'name'     => esc_html__( 'Templatespare', 'newsium' ),
+                'slug'     => 'templatespare',
+                'required' => false,
+            ),
+            array(
+                'name'     => esc_html__( 'Elespare', 'newsium' ),
+                'slug'     => 'elespare',
+                'required' => false,
+            ),
+            array(
+                'name'     => esc_html__( 'Blockspare', 'newsium' ),
+                'slug'     => 'blockspare',
+                'required' => false,
+            ),
+            array(
+                'name'     => esc_html__( 'Latest Posts Block', 'newsium' ),
+                'slug'     => 'latest-posts-block-lite',
+                'required' => false,
+            ),
+            array(
+                'name'     => esc_html__( 'Magic Content Box', 'newsium' ),
+                'slug'     => 'magic-content-box-lite',
+                'required' => false,
+            ),
+            array(
+                'name'     => esc_html__( 'WP Post Author', 'newsium' ),
+                'slug'     => 'wp-post-author',
+                'required' => false,
+            ),
+            array(
+                'name'     => esc_html__( 'Free Live Chat using 3CX', 'newsium' ),
+                'slug'     => 'wp-live-chat-support',
+                'required' => false,
+            )
+        );
+
+        tgmpa( $plugins );
+
+    }
+
+endif;
+
+add_action( 'tgmpa_register', 'newsium_recommended_plugins' );
